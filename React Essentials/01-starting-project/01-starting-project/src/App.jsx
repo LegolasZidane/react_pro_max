0507dc674e84a8b596768109c68a5e9247877ac8
LegolasZidane <laraibaa6@gmail.com>
2025-07-27 04:49:42 +0530

    react essentials props

--- React Essentials/01-starting-project/01-starting-project/src/App.jsx
+++ React Essentials/01-starting-project/01-starting-project/src/App.jsx
@@ -1,16 +1,40 @@
+import componentsImage from './assets/components.png';
+import { CORE_CONCEPTS } from './data';
+
+function CoreConcept({image, title, description}){
+  return (
+    <li>
+      <img src={image} alt={title} />
+      <h3>{description}</h3>
+      <p>{title}</p>
+    </li>
+  );
+}
+
 function App() {
   return (
     <div>
-      <header>
-        <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
-        <h1>React Essentials</h1>
-        <p>
-          Fundamental React concepts you will need for almost any app you are
-          going to build!
-        </p>
-      </header>
+      <Header />
       <main>
-        <h2>Time to get started!</h2>
+        <section id="core-concepts">
+          <h2>Core Concepts</h2>
+          <ul>
+           <CoreConcept 
+              title={CORE_CONCEPTS[0].title} 
+              description={CORE_CONCEPTS[0].description}
+              image={CORE_CONCEPTS[0].image} />
+           <CoreConcept 
+              {...CORE_CONCEPTS[1]} />
+            <CoreConcept 
+              title={CORE_CONCEPTS[2].title} 
+              description={CORE_CONCEPTS[2].description}
+              image={CORE_CONCEPTS[2].image} />
+            <CoreConcept 
+              title={CORE_CONCEPTS[3].title} 
+              description={CORE_CONCEPTS[3].description}
+              image={CORE_CONCEPTS[3].image} />
+          </ul>
+        </section>
       </main>
     </div>
   );
