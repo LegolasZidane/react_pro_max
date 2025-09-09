e6dc556a49b42904a5f1b3f22f8b2e9c173c01b5
LegolasZidane <laraibaa6@gmail.com>
2025-09-10 03:32:35 +0530

    setting up files for roi project

---
+++ React Essentials - Deep Dive/ROI Project/src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div id='header'>
+      <img src="./investment-calculator-logo.png" alt="Money Money Money"/>
+      <h1>Investment Calculator</h1>
+    </div>
+  )
+}
+
+export default App
