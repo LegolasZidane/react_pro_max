cccb6e24f45b35ba48aeef95d97f5772650602b7
LegolasZidane <laraibaa6@gmail.com>
2025-09-14 04:30:49 +0530

    trying

--- Refs and Portals/Project Management/src/App.jsx
+++ Refs and Portals/Project Management/src/App.jsx
@@ -1,7 +1,11 @@
+import Sidebar from "./components/Sidebar";
+import Content from "./components/Content";
+
 function App() {
   return (
     <>
-      <h1 className="my-8 text-center text-5xl font-bold">Hello World</h1>
+      <Sidebar />
+      <Content />
     </>
   );
 }
---
+++ Refs and Portals/Project Management/src/components/Sidebar.jsx
@@ -0,0 +1,11 @@
+export default function Sidebar(){
+    return (
+        <section className="w-1/4 h-screen text-center bg-black text-white" id="sidebar">
+            <p className="py-4 my-4">Your Projects</p>
+            <button>+ Add Project</button>
+            <ul>
+                    
+            </ul>
+        </section>
+    );
+}
