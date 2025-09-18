11238f915e439eec0365cd428ed1c89f9db9b606
LegolasZidane <laraibaa6@gmail.com>
2025-09-18 22:03:41 +0530

    restart

--- Refs and Portals/Project Management/src/App.jsx
+++ Refs and Portals/Project Management/src/App.jsx
@@ -1,33 +1,9 @@
-import { useState } from "react";
-import Sidebar from "./components/Sidebar.jsx";
-import Content from "./components/Content";
+import ProjectsSidebar from "./components/ProjectsSidebar.jsx";
 
-function App() {
-
-  const [ addClicked, setAddClicked ] = useState(false);
-  const [ projectClicked, setProjectClicked ] = useState(false);
-
-  function handleAddClicked(){
-    setAddClicked(prev => prev = true);
-  }
-
-  function handleCancelClicked(){
-    setAddClicked(prev => prev = false);
-  }
-
-  return (
-    <div className="h-screen w-screen flex flex-col">
-      <div className="h-[5%] w-screen"></div>
-      <section className="h-[95%] flex" id="loading-page">
-        <Sidebar onClickAdd={handleAddClicked}/>
-        <Content 
-          addPressed={addClicked}
-          projectPressed={projectClicked}
-          onClickCancel={handleCancelClicked}
-        />
-      </section>
-    </div>
+export default function App(){
+  return( 
+    <main className="h-screen my-8">
+      <ProjectsSidebar />
+    </main> 
   );
 }
-
-export default App;
