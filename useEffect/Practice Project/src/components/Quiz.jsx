a796026ffaab85014cdd09b5cf4093c0597a41d1
LegolasZidane <laraibaa6@gmail.com>
2025-09-23 00:17:23 +0530

    Done with the Practice Project

--- useEffect/Practice Project/src/components/Quiz.jsx
+++ useEffect/Practice Project/src/components/Quiz.jsx
@@ -1,8 +1,8 @@
 import { useState, useCallback } from "react";
 
 import QUESTIONS from "../questions.js";
-import quizCompleteImg from '../assets/quiz-complete.png';
 import Question from "./Question.jsx";
+import Summary from './Summary.jsx';
 
 export default function Quiz() {
     
@@ -21,12 +21,7 @@ export default function Quiz() {
 
     if( quizIsComplete ){
 
-        return (
-            <div id="summary">
-                <img src={quizCompleteImg} alt="Trophy Icon" />
-                <h2>Quiz Completed!</h2>
-            </div>
-        );
+        return <Summary userAnswers={userAnswers} />
 
     }
 
