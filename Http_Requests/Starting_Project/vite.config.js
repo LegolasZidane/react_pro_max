9453fb5f8bec38e02afadb5eab543d3eca3f74a4
LegolasZidane <laraibaa6@gmail.com>
2025-09-23 22:12:39 +0530

    setting up HTTP Requests module

--- Http_Requests/Starting_Project/vite.config.js
+++ Http_Requests/Starting_Project/vite.config.js
@@ -1,9 +1,7 @@
-import MillionLint from '@million/lint';
-import million from 'million/compiler';
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [million.vite({ auto: true }), react()],
-});
+  plugins: [react()],
+})
--- useEffect/Practice Project/src/App.jsx
+++ useEffect/Practice Project/src/App.jsx
@@ -1,7 +1,6 @@
 import Header from "./components/Header";
 import Quiz from "./components/Quiz";
 
-
 export default function App() {
 
     return (
