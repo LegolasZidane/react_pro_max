780d3b277f3d3d547f4386a1265b799eae3bfb59
LegolasZidane <laraibaa6@gmail.com>
2025-09-24 03:45:42 +0530

    done with http requests module

--- Http_Requests/Starting_Project/src/App.jsx
+++ Http_Requests/Starting_Project/src/App.jsx
@@ -1,17 +1,40 @@
-import { useRef, useState, useCallback } from 'react';
+import { useRef, useState, useCallback, useEffect } from 'react';
 
 import Places from './components/Places.jsx';
 import Modal from './components/Modal.jsx';
 import DeleteConfirmation from './components/DeleteConfirmation.jsx';
 import logoImg from './assets/logo.png';
 import AvailablePlaces from './components/AvailablePlaces.jsx';
+import ErrorPage from './components/ErrorPage.jsx';
+import { updateUserPlaces, fetchUserPlaces } from './http.js';
 
 function App() {
   const selectedPlace = useRef();
 
+  const [ isFetching, setIsFetching ] = useState(false);
   const [userPlaces, setUserPlaces] = useState([]);
-
+  const [ error, setError ] = useState();
+  const [ errorUpdatingPlaces, setErrorUpdatingPlaces ] = useState();
   const [modalIsOpen, setModalIsOpen] = useState(false);
+
+  useEffect(() => {
+
+    async function fetchPlaces() {
+      setIsFetching(true);
+      try{
+        
+        const places = await fetchUserPlaces();
+        setUserPlaces(places);
+
+      } catch(error){
+        setError({ message: error.message || 'Failed to fetch user places.' });
+      }
+      setIsFetching(false);
+    }
+
+    fetchPlaces();
+
+  }, []);
 
   function handleStartRemovePlace(place) {
     setModalIsOpen(true);
@@ -22,7 +45,9 @@ function App() {
     setModalIsOpen(false);
   }
 
-  function handleSelectPlace(selectedPlace) {
+  async function handleSelectPlace(selectedPlace) {
+    // await updateUserPlaces([selectedPlace, ...userPlaces]); This could work/but look stuck.
+    //This is called optimistic loading
     setUserPlaces((prevPickedPlaces) => {
       if (!prevPickedPlaces) {
         prevPickedPlaces = [];
@@ -32,6 +57,15 @@ function App() {
       }
       return [selectedPlace, ...prevPickedPlaces];
     });
+
+    try{
+      
+      await updateUserPlaces([selectedPlace, ...userPlaces]);
+    
+    } catch(error){
+      setUserPlaces(userPlaces);
+      setErrorUpdatingPlaces({ message: error.message || 'Failed to update places.'});
+    }
   }
 
   const handleRemovePlace = useCallback(async function handleRemovePlace() {
@@ -39,11 +73,34 @@ function App() {
       prevPickedPlaces.filter((place) => place.id !== selectedPlace.current.id)
     );
 
+    try{
+      
+      await updateUserPlaces(userPlaces.filter((place) => place.id !== selectedPlace.current.id));
+
+    } catch(error){
+      setUserPlaces(userPlaces);
+      setErrorUpdatingPlaces({ message: error.message || 'Failed to delete place.'});
+    }
+
     setModalIsOpen(false);
-  }, []);
+  }, [userPlaces]);
+
+  function handleError() {
+    setErrorUpdatingPlaces(null);
+  }
 
   return (
     <>
+      <Modal open={errorUpdatingPlaces} onClose={handleError}>
+        {errorUpdatingPlaces && (
+          <ErrorPage 
+            title="An error occured!"
+            message={errorUpdatingPlaces.message}
+            onConfirm={handleError}
+          />
+        )}
+      </Modal>
+
       <Modal open={modalIsOpen} onClose={handleStopRemovePlace}>
         <DeleteConfirmation
           onCancel={handleStopRemovePlace}
@@ -60,12 +117,15 @@ function App() {
         </p>
       </header>
       <main>
-        <Places
+        {error && <ErrorPage title="An error occurred!" message={ error.message } />}
+        {!error && <Places
           title="I'd like to visit ..."
           fallbackText="Select the places you would like to visit below."
+          isLoading={isFetching}
+          loadingText="Fetching your places..."
           places={userPlaces}
           onSelectPlace={handleStartRemovePlace}
-        />
+        />}
 
         <AvailablePlaces onSelectPlace={handleSelectPlace} />
       </main>
--- Http_Requests/Starting_Project/src/components/AvailablePlaces.jsx
+++ Http_Requests/Starting_Project/src/components/AvailablePlaces.jsx
@@ -1,11 +1,61 @@
+import { useState, useEffect } from 'react';
+
 import Places from './Places.jsx';
+import ErrorPage from './ErrorPage.jsx';
+import { sortPlacesByDistance } from '../loc.js';
+import { fetchAvailablePlaces } from '../http.js';
 
 export default function AvailablePlaces({ onSelectPlace }) {
+  //this is a convention for fetching data
+  const [ isFetching, setIsFetching ] = useState(false);
+  const [ availablePlaces, setAvailablePlaces ] = useState([]);
+  const [ error, setError ] = useState();
+
+  useEffect(() => {
+
+    async function fetchPlaces() {
+
+      setIsFetching(true);
+
+      try {
+
+        const places = await fetchAvailablePlaces();
+
+        navigator.geolocation.getCurrentPosition((position) => {
+
+          const sortedPlaces = sortPlacesByDistance(
+            places, 
+            position.coords.latitude, 
+            position.coords.longitude,
+          );
+          setAvailablePlaces(sortedPlaces);
+          setIsFetching(false);
+        });
+
+
+      } catch(error){
+        
+        setError({message: error.message || 'Could not fetch places, please try again later.'});
+        setIsFetching(false);
+
+      }
+      
+    }
+
+    fetchPlaces();
+  }, []);
+
+  if( error ){
+    return <ErrorPage title="An error occured!" message={error.message} />
+  }
+
   return (
     <Places
       title="Available Places"
-      places={[]}
+      places={availablePlaces}
       fallbackText="No places available."
+      isLoading={isFetching}
+      loadingText="Fetching place data..."
       onSelectPlace={onSelectPlace}
     />
   );
