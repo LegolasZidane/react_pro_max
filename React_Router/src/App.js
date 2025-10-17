b00dd6f6212e9e9f8afc5db820d7f492c9d21068
LegolasZidane <laraibaa6@gmail.com>
2025-10-18 01:26:39 +0530

    reached NavLink

---
+++ React_Router/src/App.js
@@ -0,0 +1,44 @@
+import {
+  createBrowserRouter,
+  // createRoutesFromElements,
+  RouterProvider,
+  // Route,
+} from "react-router-dom";
+
+import HomePage from "./pages/HomePage.js";
+import ProductsPage from "./pages/ProductsPage.js";
+import RootLayout from "./pages/RootLayout.js";
+import ErrorPage from "./pages/ErrorPage.js";
+
+// https://example.com/products
+//https-> protocol
+//example.com->domain name
+// /products->path
+// even / is a path
+
+// const routeDefinitions = createRoutesFromElements(
+//   <Route>
+//     <Route path="/" element={<HomePage />}/>
+//     <Route path="/products" element={<ProductsPage />}/>
+//   </Route>
+// );
+
+// const router = createBrowserRouter(routeDefinitions);
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <RootLayout />,
+    errorElement: <ErrorPage />,
+    children: [
+      { path: "/", element: <HomePage /> },
+      { path: "/products", element: <ProductsPage /> },
+    ],
+  },
+]);
+
+function App() {
+  return <RouterProvider router={router} />;
+}
+
+export default App;
